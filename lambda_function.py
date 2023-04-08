c4515961c100474e067457d96d59b3c456109f17
smelehy <sam.melehy@gmail.com>
2023-04-08 10:18:27 -0700

    init data tables and fields

--- lambda_function.py
+++ lambda_function.py
@@ -42,7 +42,16 @@ rds_host  = 'hlw-database-1.csmymcm2btd4.us-east-2.rds.amazonaws.com'
 name      = dbsecrets.get('username')
 password  = dbsecrets.get('password')
 #db_name   = dbparams.get('dbClusterIdentifier')
-db_name    = 'hlw_database_1'
+db_name   = 'hlw_database_1'
+sqltables = {
+    'sysconfig' : {
+        'id'      : 
+        'appname' : {'type': 'varchar(100)', 'value'    : 'hellow-lamdba-app'},
+        'sysid'   : {'type': 'int'         , 'value'    : 123876             },
+        'country' : {'type': 'varchar(10)' , 'value'    : "USA"              },
+        'created' : 
+    },
+}
 
 def lambda_handler(event, context):
     #result = {**dbsecrets, **dbparams}
@@ -51,9 +60,14 @@ def lambda_handler(event, context):
     conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
     print('after connect')
     cur  = conn.cursor()
-    print('after cursor')
-    item_count = 0
-    result = {}
+    sqltext = 'CREATE TABLE IF NOT EXISTS sysconfig (id INT AUTO_INCREMENT PRIMARY KEY,
+    appname VARCHAR(255) DEFAULT 'hello-world-lambda-app',
+    sysid AUTO_INCREMENT,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description TEXT DEFAULT 'This is the first lambda serverless app',
+    last_update DATE
+);'
+    cur.execute(sqltext)
     cur.execute('select * from sysconfig')
     for row in cur:
         print(row,row[0],row[1],row[2])
